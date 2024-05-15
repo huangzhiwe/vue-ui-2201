742308a5793d31c358c4afdc01452ab13e92493e
huangzhiwei <2129879505@qq.com>
2024-05-15 16:09:25 +0800

    用户信息

--- src/components/UserInfoComp.vue
+++ src/components/UserInfoComp.vue
@@ -33,17 +33,54 @@
               </ElDropdownItem>
               <ElDropdownItem divided command="home"><el-icon><Management /></el-icon>网站首页</ElDropdownItem>
               <ElDropdownItem command="main"><el-icon><Management /></el-icon>用户首页</ElDropdownItem>
+              <ElDropdownItem divided command="userlogo">修改用户头像</ElDropdownItem>
+              <ElDropdownItem command="userinfo">修改用户附加信息</ElDropdownItem>
               <ElDropdownItem divided command="logout">安全退出</ElDropdownItem>
             </ElDropdownMenu>
           </template>
         </ElDropdown>
       </div>
-      <div v-else>需要登录</div>
+      <div v-else>
+        <el-button @click="router.push('/user/login')">登录</el-button>
+        </div>
     </div>
+  
   </div>
+  <ElDialog v-model="mvisible" title="修改用户附加信息" @closed="update">
+    <div>
+      <ElForm>
+        <ElFormItem label="姓名">
+          <ElInput v-model="muserinfo.nickname" />
+        </ElFormItem>
+        <ElFormItem label="简介">
+          <ElInput v-model="muserinfo.info" />
+        </ElFormItem>
+        <ElFormItem label="QQ">
+          <ElInput v-model="muserinfo.qq" />
+        </ElFormItem>
+        <ElFormItem label="微信">
+          <ElInput v-model="muserinfo.wechat" />
+        </ElFormItem>
+        <ElFormItem label="性别">
+          <ElRadioGroup v-model="muserinfo.sex">
+          <ElRadio value="m">男</ElRadio>
+          <ElRadio value="f">女</ElRadio>
+          <ElRadio value="n">保密</ElRadio>
+          </ElRadioGroup>
+        </ElFormItem>
+      </ElForm>      
+    </div>
+    <template #footer>
+      <ElButton @click="mvisible=false">关闭</ElButton>
+      <ElButton type="primary"@click="modifyUser">保存</ElButton>
+    </template>
+
+</ElDialog>
 </template>
 
 <script setup>
+
+//#region 基础导入
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
@@ -55,19 +92,86 @@ import {
   ElDropdown,
   ElDropdownMenu,
   ElDescriptionsItem,
+  ElMessage,
+  ElDialog,
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElRadioGroup,
+  ElRadio,
 } from 'element-plus';
 const { info, ipinfo, weather } = storeToRefs(ipstore());
 const { isLogin, user } = storeToRefs(useStore());
+const{update}=useStore();
 
 const router = useRouter();
+//#endregion
 
+//#region 菜单处理
 const handleMenu =(command)=>{
   console.log('菜单:',command);
   if(command == 'home'){
     router.push('/');
+  } else if(command == 'main'){
+    router.push('/user/main');
+  } else if(command == 'logout'){
+    logout();
+  }else if(command == 'userinfo'){
+    showModifyUser();
   }
   
 }
+//#endregion
+
+
+//#region 修改用户附加信息
+const mvisible = ref(false);
+const muserinfo = ref({});
+
+const showModifyUser=()=>{
+  //只要相关字段
+  muserinfo.value = {
+    nickname:user.value.tbUserInfo.nickname,
+    sex:user.value.tbUserInfo.sex,
+    qq:user.value.tbUserInfo.qq,
+    wechat:user.value.tbUserInfo.wechat,
+    info:user.value.tbUserInfo.info,
+  };
+
+  mvisible.value = true;
+
+};
+
+const modifyUser=()=>{
+  Api.put('/user',muserinfo.value,(data)=>{
+    if(data.success){
+      ElMessage.success(data.message);
+    }else{
+      ElMessage.error(data.message);
+    }
+  })
+};
+
+
+
+//#endregion
+
+
+
+//#region 退出登录
+
+const logout = () => {
+  Api.delete('/auth',{},()=>{
+    ElMessage.success('退出成功');
+    update();
+  });
+};
+
+
+
+//#endregion
+
 </script>
 
 <style scoped>
